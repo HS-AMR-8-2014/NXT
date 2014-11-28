259edfd817a0b8c08d407cd0a0daf04309c6ce62
adrianorzyszek <adrian.orzyszek11@gmail.com>
2014-11-28 14:37:06 +0100

    test linesensvalue

--- src/parkingRobot/hsamr8/GuidanceAT.java
+++ src/parkingRobot/hsamr8/GuidanceAT.java
@@ -423,6 +423,8 @@ public class GuidanceAT {
 		LCD.drawString("X (in cm): " + (navigation.getPose().getX()*100), 0, 0);
 		LCD.drawString("Y (in cm): " + (navigation.getPose().getY()*100), 0, 1);
 		LCD.drawString("Phi (grd): " + (navigation.getPose().getHeading()/Math.PI*180), 0, 2);
+		LCD.drawString("LineSenL: " + (perception.getLeftLineSensorValue()), 0, 3);
+		LCD.drawString("LineSenR: " + (perception.getRightLineSensorValue()), 0, 4);
 		
 //		perception.showSensorData();
 		
